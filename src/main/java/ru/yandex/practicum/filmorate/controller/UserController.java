2fe60a1836f2af53510fabcf5bb615381e6f8049
trofimovichp <trofimovichp@yandex.by>
2023-05-23 23:55:30 +0300

    fix7

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,8 +8,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @Slf4j
